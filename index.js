d61dec2a139439b57edb5197db14ee316c3b396f
sh073038 <139067226+sh073038@users.noreply.github.com>
2023-07-28 09:06:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const express = require('express');
-const port = process.env.PORT || 8000;
-const db = require("./config/configdb");
-const Doctor = require('./models/doctor');
-const app = express();
-app.use(express.urlencoded());
-app.use(express.json());
-app.use('/', require('./router/index'));
-app.listen(port, (error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is running");
+const Doctor = require('../models/doctor');
+const jwt = require('jsonwebtoken');
+// this is welcome router function
+module.exports.home = async function (req, res) {
+    return res.send("<h1>welcome to hospital api || </h1>");
+}
+// this is register function
+module.exports.Register = async function (req, res) {
+    try {
+        let doctorPresent = await Doctor.find({ email: req.body.email });
+        let doctorRegister = doctorPresent;
+        if (doctorPresent.length == 0) {
+            doctorPresent = new Doctor(req.body);
+            doctorRegister = await doctorPresent.save();
+        }
+        return res.send(200).json({
+            message: "Doctor Register!!",
+            doctor: doctorRegister
+        })
+    } catch (error) {
+        return res.send("Error in registation !!");
     }
-});
+}
+
+module.exports.Login = async function token(req, res) {
+    try {
+        const checkDoctor = await Doctor.findOne({ email: req.body.email });
+        if (checkDoctor) {
+            let token = jwt.sign(checkDoctor.toJSON(), 'hospitalApi', { expiresIn: 6000 * 6000 });
+            return res.json(200, {
+                message: "Login SuccessFully !!",
+                token: token
+            })
+        } else {
+            return res.send("Email or Password are not correct !!");
+        }
+    } catch (error) {
+        return res.send("Error in Login !!");
+    }
+}
